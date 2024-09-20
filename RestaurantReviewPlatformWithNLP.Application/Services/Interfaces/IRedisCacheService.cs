7193d6838c45c96c85e1b6c5cfcb615aabbbeb7c
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-20 22:31:27 +0500

    RedisCacheService interface;

---
+++ RestaurantReviewPlatformWithNLP.Application/Services/Interfaces/IRedisCacheService.cs
@@ -0,0 +1,13 @@
+﻿using RestaurantReviewPlatformWithNLP.Application.DTOs;
+using StackExchange.Redis;
+
+namespace RestaurantReviewPlatformWithNLP.Application.Services.Interfaces
+{
+    public interface IRedisCacheService
+    {
+        Task<ResponseDTO<bool>> SetLeaderboardScoreAsync(Guid restaurantId, decimal score);
+        Task<ResponseDTO<double?>> GetLeaderboardScoreAsync(Guid restaurantId);
+        Task<ResponseDTO<SortedSetEntry[]>> GetTopRestaurantsAsync(int count);
+        Task<ResponseDTO<bool>> RemoveRestaurantFromLeaderboardAsync(Guid restaurantId);
+    }
+}
