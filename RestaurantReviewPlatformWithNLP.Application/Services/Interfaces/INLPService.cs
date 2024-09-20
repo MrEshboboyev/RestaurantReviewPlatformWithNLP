7c4b8ad469283f3ca01e5af0ec423e56c9788a20
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-20 20:54:56 +0500

    INLPService interface ;

---
+++ RestaurantReviewPlatformWithNLP.Application/Services/Interfaces/INLPService.cs
@@ -0,0 +1,7 @@
+﻿namespace RestaurantReviewPlatformWithNLP.Application.Services.Interfaces
+{
+    public interface INLPService
+    {
+        Task<float> AnalyzeSentimentAsync(string text);
+    }
+}
