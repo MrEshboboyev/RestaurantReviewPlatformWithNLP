f708bb03347527a2020da9440a57687d804d412c
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-20 21:04:39 +0500

    Add sentiment score field to Review.cs entity;

--- RestaurantReviewPlatformWithNLP.Domain/Entities/Review.cs
+++ RestaurantReviewPlatformWithNLP.Domain/Entities/Review.cs
@@ -7,6 +7,7 @@
         public string UserId { get; set; }
         public string ReviewText { get; set; }
         public decimal Rating { get; set; }
+        public float SentimentScore { get; set; }
         public DateTime CreatedAt { get; set; }
 
         // Navigation Property
---
+++ RestaurantReviewPlatformWithNLP.Infrastructure/Data/Migrations/20240920160308_addSentimentScoreToReviews.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantReviewPlatformWithNLP.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSentimentScoreToReviews : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "SentimentScore",
+                table: "Reviews",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SentimentScore",
+                table: "Reviews");
+        }
+    }
+}
