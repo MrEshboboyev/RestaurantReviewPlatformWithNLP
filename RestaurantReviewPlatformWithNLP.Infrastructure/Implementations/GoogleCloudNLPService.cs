4c00b41378d652ef79b110f507254461ac223efb
MrEshboboyev <mreshboboyev@gmail.com>
2024-09-20 21:00:57 +0500

    GoogleCloudNLPService created ; Adding package to Infrastructure: Google.Cloud.Language.V1;

---
+++ RestaurantReviewPlatformWithNLP.Infrastructure/Implementations/GoogleCloudNLPService.cs
@@ -0,0 +1,29 @@
+﻿using Google.Cloud.Language.V1;
+using RestaurantReviewPlatformWithNLP.Application.Services.Interfaces;
+
+namespace RestaurantReviewPlatformWithNLP.Infrastructure.Implementations
+{
+    public class GoogleCloudNLPService : INLPService
+    {
+        private readonly LanguageServiceClient _languageServiceClient;
+
+        public GoogleCloudNLPService()
+        {
+            _languageServiceClient = LanguageServiceClient.Create();
+        }
+
+        public async Task<float> AnalyzeSentimentAsync(string text)
+        {
+            try
+            {
+                var document = Document.FromPlainText(text);
+                var response = await _languageServiceClient.AnalyzeSentimentAsync(document);
+                return response.DocumentSentiment.Score;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
